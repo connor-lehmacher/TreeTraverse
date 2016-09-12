5cd8083e57e0e2f103a12e6e0543717ab52c6bf4
Connor Lehmacher <clehmac@hotmail.com>
2016-09-12 09:57:39 -0400

    Implement Graphs

---
+++ src/treeBuilder/Graph.java
@@ -0,0 +1,15 @@
+package treeBuilder;
+
+import java.util.ArrayList;
+
+public class Graph {
+	ArrayList<Node> nodes = new ArrayList<Node>();
+	
+	public Graph(ArrayList<Node> nodes) {
+		this.nodes = nodes;
+	}
+	
+	public void print(){
+		nodes.get(0).print();
+	}
+}
